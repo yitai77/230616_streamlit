919be265145d7157518779a2403ba08d4fb767f3
{yitai77} <{yitai7@naver.com}>
2023-06-16 10:58:11 +0900

    함수

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st # streamlit -> import(가져오기) -> as (st 이름)
 
 # streamlit run app.py
 
-st.title("나의 파이썬 웹 페이지")
+st.title("노형석 오늘 뭐하냐? ㅋ")
 st.header("수업 8일차에 만들었어요")
 st.subheader("그래도 잘 만들었죠?")
 st.write("내가 만든 streamlit 페이지, 너를 위해 구웠지")
