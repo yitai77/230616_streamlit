2fa51db81756c207460b070f2e4bccafb23ee5e7
{yitai77} <{yitai7@naver.com}>
2023-06-16 09:41:23 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# pip install streamlit
+# streamlit hello
+# ctrl + c: 실행종료
+
+import streamlit as st # streamlit -> import(가져오기) -> as (st 이름)
+# st라는 변수명으로 streamlit의 기능들을 사용하겠다
+
+st.title("나의 파이썬 웹 페이지")
+st.header("수업 8일차에 만들었어요")
+st.subheader("그래도 잘 만들었죠?")
+st.write("내가 만든 streamlit 페이지, 너를 위해 구웠지")
